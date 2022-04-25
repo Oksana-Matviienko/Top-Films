cfc1c2c0d846c2d93d48e34bee5bff811d9d905e
Oksana <7564211@gmail.com>
2022-04-25 21:31:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { data } from "./data"
 
 function App() {
+  const [films, setFilms] = useState(data);
+  const [showMore, setShowMore] = useState (false);
+  const removeFilm = (id) =>{
+    let newFilms = films.filter(item => item.id !==id);
+    setFilms(newFilms);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <div className='container'>
+    <h1>Top {films.length} best films</h1>
+    </div>
+    {films.map((element =>{
+      const {id, title, year, country, image, description} = element;
+      return(
+        <div key={id}>
+          <div className='container'>
+            <h2>{id}. {title}</h2>
+          </div>
+          <div className='container'>
+            <p>Year: {year}</p>
+          </div>
+          <div className='container'>
+            <p>{country}</p>
+          </div>
+          <div className='container'>
+<img src={image} alt="films"/>
+          </div>
+          <div className='container description'>
+            <p>{showMore ? description : description.substring(0,300) + "..."}
+            <button className='showmore' onClick={() => setShowMore(!showMore)}>{showMore? "Show less" : "Show more"}</button></p>
+          </div>
+          <div className='container'>
+<button className='btn' onClick ={()=> removeFilm(id)}>I'v seen it. Delete this one</button>
+          </div>
+        </div>
+      )
+      }))}
+      <div className='container'>
+        <button className='btn delete' onClick={()=> setFilms([])}>Delete all</button>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,82 @@
+export const data=[
+   {
+      id: 1 ,
+      title:"The Green Mile" ,
+      year: 1999 ,
+      country: "USA" ,
+      image: "https://paramountcomedy.ru/upload/medialibrary/8e8/8e8905ec6b206b0e27d765aed3eff672.webp",
+      description:`Based on the Stephen King novel of the same name, the film tells the story of prison warden Paul from the E Block of Cold Mountain. In this unit, criminals await their death penalty and walk down a corridor painted green for their final journey, hence the title of the piece. A new inmate is brought to the prison--an African American with the muscles of Goliath, the strength of an elephant, and the mind of a child--John Coffey. He is accused of raping and murdering two little girls. The case is closed, the punishment imminent, but the weeping death row inmate shows such overpowering kindness and sincere love for every earthly creature that he makes the warden seriously question his guilt and begin to investigate the case himself.`
+   },
+   {
+      id: 2,
+      title: "Pulp Fiction",
+      year: 1994,
+      country: "USA",
+      image: "https://paramountcomedy.ru/upload/medialibrary/ab1/ab1f41c72cc7fae63511e43e627d8e47.webp" ,
+      description:`The film is divided into six parts, which follow each other in no chronological order and show, in general, three stories. The first is about gangsters Jules and Vincent. Vincent takes a cocaine-addicted mobster's wife to a dance, and Jules escapes bullets and discovers a divine sign. The second is about a boxer, Butch, who breaks an agreement with the same mobster and gets into trouble over his father's gold watch. The third is about two burglars who get in the way of the bandits from the first story. All of these events, though related in meaning, are still more related in form. After all, it doesn't matter what the trigger was pulled for if it results in someone's brains being sprayed all over the interior of a car or the demise of someone who came out of the toilet at an unnecessary moment. It especially doesn't matter if it's funny, aesthetic, and Tarantino-esque`
+   },
+   {
+      id: 3,
+      title: "Forrest Gump",
+      year: 1994,
+      country:"USA" ,
+      image:"https://paramountcomedy.ru/upload/medialibrary/142/142a05bff2ebd15e4bba739045bf965a.webp" ,
+      description:`Forrest Gump is a boy with an IQ of 75, which according to the modern American classification is characteristic of people with borderline mental retardation. But despite this, the main character's future is not as dramatic as it might seem. Through his perseverance and daily training, Forrest gains the ability to run fast (first to run away from bullies and then to get an athletic scholarship to attend college). Fate takes him to the war and the bleachers of the rally, table tennis, and the shrimping business. All in all, the two-and-a-half hour film tells the audience a life-affirming tale with a truly therapeutic effect and a return of the will to live.`
+   },
+   {
+      id: 4,
+      title: "Dead Poets Society",
+      year: 1989,
+      country:"USA" ,
+      image: "https://paramountcomedy.ru/upload/medialibrary/988/988cbb50f3ede7d0262cc42d065f94ed.webp" ,
+      description:`When freedom-loving literature teacher John Keating arrives at conservative Wellton College with an unconventional approach to teaching, he immediately has his admirers (students) and haters (other teachers). He opposes the traditional method of "cutting all students into one row" with his vision of education, namely, the education of an independent and harmonious personality. Instead of reading from memory, Keating recites from memory, encourages students to argue among themselves (on the topic, of course), discusses the transience of life, and frames literature not as a collection of tropes and artistic devices but as an outburst of feeling and emotion. But inadvertently stuttering about the "Society of Dead Poets" and giving the son of a failed military man advice to follow his dream, the teacher does not anticipate the consequences.`
+   },
+   {
+      id: 5,
+      title:"Fight Club" ,
+      year: 1999,
+      country: "USA, Germany",
+      image: "https://paramountcomedy.ru/upload/medialibrary/fa8/fa8ec1b92250b15ff1e8be4762cf7a8d.webp",
+      description:`The last breakthrough of the countercultural era of the 20th century, a spit in the direction of conformism, capitalism and consumer society, which for a short time pulls the viewer out of his comfort zone, shakes him well and offers to live on with this feeling of unfinished and wrongness of life - this is what this film is all about. The picture is based on the novel of the same name by Chuck Palahniuk. The main character is a man with no name, a gray mass with insomnia and an uncontrollable desire to collect pennies to buy furniture from IKEA. He is in his late 30s and goes to cancer groups to assert himself at the expense of other people's suffering. But one day Tyler Durden, a soap salesman, anarchist and non-conformist, bursts into his life. Together they create a "fight club," a place to vent their aggression. And then they come up with and implement the "Rumble" project to "clean up the world".`
+   },
+   {
+      id: 6,
+      title: "Good Will Hunting",
+      year: 1997,
+      country: "USA",
+      image: "https://paramountcomedy.ru/upload/medialibrary/ea8/ea801d0d036d233bafb3c4cc5ecbbea7.webp",
+      description: `Will is a child prodigy with an eidetic memory. He reads books with gusto, memorizing literally every line. He doesn't want to get a college degree, but instead mops floors at MIT and drinks beer with his friends after work. Something in his childhood broke his psyche, and now he spews all his pain in scuffles. Pulling the hero out of the maelstrom of doom is math professor Lambeau. He takes him from the hands of justice and turns him over to his psychotherapist friend Maguire. Having defeated Will in a battle of wit, the doctor gradually begins to guess at the reasons for such fierce self-destruction, and in passing also copes with his own depression.`
+   },
+   {
+      id: 7,
+      title: "The Shawshank Redemption",
+      year: 1994,
+      country: "USA",
+      image: "https://paramountcomedy.ru/upload/medialibrary/de2/de2a66ef5575cfe7c76b8b6542cb6af0.webp",
+      description: `Bank vice president Andy Dufresne is imprisoned in New England's harshest and darkest prison, Shawshank, for a crime he did not commit. He must spend the rest of his life surrounded by sex-crazed musclemen, brutal wardens, murderers, and corrupt bosses. But Andy with true nobility endures the trials of hard work, beatings, bullying, hazing, and even being turned out by his But in all this filth he does not lose his identity, nor does he lose his hope of escape by taking apart the wall in his prison cell piece by piece.`
+   },
+   {
+      id: 8,
+      title: "American Beauty",
+      year: 1999,
+      country: "USA",
+      image: "https://paramountcomedy.ru/upload/medialibrary/e6a/e6adf5e478c871dcd436e63e3b767337.webp",
+      description: `The plot of the movie revolves around a fairly well-to-do average American Burnham family. Lester, 42, has a beautiful wife (who does not love him), a daughter (who thinks he is a loser), a big house (which he does not want to return to) and a high-paying job as an advertising agent (which he hates). He believes that all his life he has been a hypocrite and has done what was expected of him, not what he really wanted. His midlife crisis pushes him to do daring and reckless things - Lester falls in love with his daughter's attractive classmate and starts buying marijuana from the son of his new homophobic neighbor.`
+   },
+   {
+      id: 9,
+      title: "Inception",
+      year: 2010,
+      country: "USA, Great Britain",
+      image: "https://paramountcomedy.ru/upload/medialibrary/6ed/6ed65ee8af2e253da8b3a87080714e22.webp",
+      description: `In the film's universe, stealing other people's ideas right out of their heads is the norm. People engaged in industrial espionage infiltrate the dreams of businessmen and steal their intellectual property. The latter, in turn, pay the guards to patrol subconscious "territory. Dominic Cobb is a high-class spy who once lost his wife and now wants to retire. Unfortunately, he can only earn his rest by completing the most difficult assignment in the history of world crime - introducing an idea into the head of the son of an energy tycoon. To do this, the hero recruits a team and sets out on a surreal journey. They can be hindered by the businessman himself, who realizes what's going on, a frenzied projection of Cobb's wife and the possibility of falling into a deadly limbo - the last level of dreams in which you can't buy a return ticket.`,
+   },
+   {
+      id: 10,
+      title: "1+1",
+      year: 2011,
+      country: "France",
+      image: "https://paramountcomedy.ru/upload/medialibrary/23a/23a5c3a618076e8a7e8eb6b3771f2760.webp",
+      description: `The film, based on real events, tells the unusual story of the friendship between the French aristocrat Philippe and the Senegalese native Driss. They meet for the first time when the paralyzed Philippe is looking for a nurse. The housekeeper no longer expects anyone to stay with them for more than two weeks, because Philippe is annoyed by literally every candidate. But suddenly, the carefree, slightly gruff and perpetually smiling Driss arouses the man's interest. The guy is the only one who doesn't treat Phillip like a child, constantly forgets about his disability and jokes about seemingly forbidden topics. Driss brings back Philip's love of life. He convinces him to move from his "safe car" to a stylish sports car, to meet his pen pal, and to go for an ear massage. And Philip, in turn, helps Driss step away from his criminal dealings and sell a painting painted in the style of conceptual art.`
+   }
+]
